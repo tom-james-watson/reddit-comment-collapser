41f52b23071000b88fafed6e023b6951d0311372
Tom Watson <tom.watson@geneity.co.uk>
2014-08-21 19:54:25 +0100

    refactor and improve initial addition of collapsers with requestAnimationFrame

--- js/comment-collapser.js
+++ js/comment-collapser.js
@@ -13,9 +13,114 @@ var colours = [
 ];
 
 
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+function make_collapser(colour, width, height) {
+    var collapser = $('<div class="collapser ' + colour + '"></div>');
+    collapser.click(toggle_collapse);
+    collapser.css({
+        'height': '-webkit-calc(100% - ' + height + 'px)',
+        'width': width
+    });
+    return collapser;
+}
 
-var observer = new MutationObserver(function(mutations) {
+
+function make_expander() {
+    var expander = $('<a href="javascript:void(0)" class="expander">[–]</a>');
+    expander.click(toggle_collapse);
+    return expander;
+}
+
+
+function add_collapser(comment) {
+	var num_child_comments;
+    var anchor_ele;
+    var depth;
+    var colour;
+    var collapser;
+    var expander;
+    var tagline;
+    var width;
+    var height;
+
+    num_child_comments = comment.find('> .child .comment').length;
+
+	if (num_child_comments > 0) {
+        depth = comment.parents('.comment').length;
+        colour = colours[depth % 10];
+
+        if (comment.hasClass('deleted')) {
+            anchor_ele = comment.children('.entry');
+        } else {
+            anchor_ele = comment.children('.midcol');
+        }
+
+        height = anchor_ele.height();
+        if (comment.hasClass('deleted')) {
+            width = '10px';
+        } else {
+            width = anchor_ele.width();
+        }
+
+        collapser = make_collapser(colour, width, height);
+		anchor_ele.append(collapser);
+
+        expander = make_expander();
+        tagline = comment.find('> .entry .tagline');
+        tagline.prepend(expander);
+	}
+
+    comment.find('> .entry .tagline .expand').remove();
+
+}
+
+
+function toggle_collapse(event) {
+	var comment = $($(this).parents('.comment')[0]);
+
+    if (comment.hasClass('collapsed')) {
+        collapse(comment);
+    }
+    else {
+        uncollapse(comment);
+    }
+}
+
+
+function collapse(comment) {
+    comment.children('.child').show(300);
+    comment.children('.midcol').show();
+    comment.removeClass('collapsed');
+    comment.addClass('noncollapsed');
+    comment.find('.expander').html('[–]');
+}
+
+
+function uncollapse(comment) {
+    comment.children('.child').hide(300);
+    comment.children('.midcol').hide();
+
+    window.setTimeout(function() {
+        comment.find('.expander').html('[+]');
+        comment.removeClass('noncollapsed');
+        comment.addClass('collapsed');
+    }, 250);
+
+    if (!elementInViewport(comment)) {	
+        $('html, body').animate({
+            scrollTop: comment.offset().top
+        }, 300);
+    }
+}
+
+
+// Test whether a given element is visible in the viewport
+function elementInViewport(element) {
+	return (element.offset().top > window.pageYOffset);
+}
+
+
+// Watch for any new comments that are loaded and add collapsers to them too
+var observer = new WebKitMutationObserver(function(mutations) {
     var once = false;
     $.each(mutations, function(index, mutation) {
         if (mutation.type === 'childList') {
@@ -24,9 +129,9 @@ var observer = new MutationObserver(function(mutations) {
                     console.log(once);
                     if (!once) {
                         once = true;
-                        add_collapsers.bind($(node.parentNode.parentNode.parentNode))();
+                        add_collapser($(node.parentNode.parentNode.parentNode));
                     }
-                    add_collapsers.bind($(node))();
+                    add_collapser($(node));
                 }
             });
         }
@@ -40,84 +145,20 @@ observer.observe(document, {
 
 
 // Add a collapser div to every non-deleted comment
-$('.comment').each(add_collapsers);
-$(document).ajaxSuccess(add_collapsers);
+var comments = $.makeArray($('.comment')).reverse();
 
-function add_collapsers() {
-
-	var collapser;
-    var anchor;
-    var expander;
-
-    if ($(this).hasClass('deleted')) {
-        anchor = $(this).children('.entry');
-    } else {
-        anchor = $(this).children('.midcol');
+function create_collapsers() {
+    var comment = comments.pop();
+    
+    if (!comment) {
+        return false;
     }
 
-	var num_child_comments = $(this).find('> .child .comment').length;
-    var depth = $(this).parents('.comment').length;
-    var colour = colours[depth % 10];
+    add_collapser($(comment));
 
-	if (num_child_comments > 0) {
-
-        var width;
-
-        if ($(this).hasClass('deleted')) {
-            width = '10px';
-        } else {
-            width = anchor.width();
-        }
-
-		collapser = $('<div class="collapser ' + colour + '"></div>');
-		collapser.css({
-			'height' : '-webkit-calc(100% - ' + anchor.height() + 'px)',
-			'width'  : width
-		});
-		anchor.append(collapser);
-
-        expander = $('<a href="javascript:void(0)" class="expander">[–]</a>');
-        $(this).find('> .entry .tagline').prepend(expander);
-
-		collapser.click(toggle_collapse);
-        expander.click(toggle_collapse);
-	}
-
-    $(this).find('> .entry .tagline .expand').remove();
-
+    requestAnimationFrame(function() {
+        create_collapsers();
+    });
 }
 
-function toggle_collapse(event) {
-
-	var comment = $($(this).parents('.comment')[0]);
-
-    if (comment.hasClass('collapsed')) {
-        comment.children('.child').show(300);
-        comment.children('.midcol').show();
-        comment.removeClass('collapsed');
-        comment.addClass('noncollapsed');
-        comment.find('.expander').html('[–]');
-    }
-    else {
-        comment.children('.child').hide(300);
-        comment.children('.midcol').hide();
-        window.setTimeout(function() {
-            comment.find('.expander').html('[+]');
-            comment.removeClass('noncollapsed');
-            comment.addClass('collapsed');
-        }, 250);
-
-        if (!elementInViewport(comment)) {	
-            $('html, body').animate({
-                scrollTop: comment.offset().top
-            }, 300);
-        }
-    }
-}
-
-// Test whether a given element is visible in the viewport
-function elementInViewport(element) {
-	var top    = element.offset().top;
-
-	return (top > window.pageYOffset);
-}
+create_collapsers();
