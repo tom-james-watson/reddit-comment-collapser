704061a3b39ce8d1fed547fe746370dae731aab6
Tom Watson <tom@tomjwatson.com>
2017-07-20 13:36:39 +0200

    add option for theme and collapse animation

--- js/comment-collapser.js
+++ js/comment-collapser.js
@@ -1,3 +1,42 @@
+function injectRetroTheme() {
+  // Create URLs for local files without hardcoding chrome-extension URL scheme
+  const styleEl = document.createElement("style");
+  const colorsPath = chrome.runtime.getURL("image/colours");
+
+  styleEl.textContent = `
+    :root body.rcc-retro div.sitetable div.comment.noncollapsed .expand:before {
+      height: calc(100% - 20px) !important;
+      background-image: url('${colorsPath}/red.png') !important;
+    }
+    :root body.rcc-retro div.sitetable div.comment.noncollapsed .comment .expand:before {
+      background-image: url('${colorsPath}/orange.png') !important;
+    }
+    :root body.rcc-retro div.sitetable div.comment.noncollapsed .comment .comment .expand:before {
+      background-image: url('${colorsPath}/dark_green.png') !important;
+    }
+    :root body.rcc-retro div.sitetable div.comment.noncollapsed .comment .comment .comment .expand:before {
+      background-image: url('${colorsPath}/blue.png') !important;
+    }
+    :root body.rcc-retro div.sitetable div.comment.noncollapsed .comment .comment .comment .comment .expand:before {
+      background-image: url('${colorsPath}/navy.png') !important;
+    }
+    :root body.rcc-retro div.sitetable div.comment.noncollapsed .comment .comment .comment .comment .comment .expand:before {
+      background-image: url('${colorsPath}/brown.png') !important;
+    }
+    :root body.rcc-retro div.sitetable div.comment.noncollapsed .comment .comment .comment .comment .comment .comment .expand:before {
+      background-image: url('${colorsPath}/army.png') !important;
+    }
+    :root body.rcc-retro div.sitetable div.comment.noncollapsed .comment .comment .comment .comment .comment .comment .comment .expand:before {
+      background-image: url('${colorsPath}/green.png') !important;
+    }
+    :root body.rcc-retro div.sitetable div.comment.noncollapsed .comment .comment .comment .comment .comment .comment .comment .comment .expand:before {
+      background-image: url('${colorsPath}/pink.png') !important;
+    }
+  `;
+
+  document.head.appendChild(styleEl);
+}
+
 // Based on: https://github.com/alicelieutier/smoothScroll
 function smoothScroll(destination) {
   let duration = 250;
@@ -35,20 +74,55 @@ window.addEventListener("click", function (ev) {
   let comment = ev.target.closest(".comment");
   let rect = comment.getBoundingClientRect();
 
-  // If top of comment is out of viewport, scroll to it
-  if (rect.top < 0) {
-    const padding = 10;
-    let scrollPosition = (window.scrollY + rect.top) - padding;
+  chrome.storage.sync.get({
+    collapseAnimation: true
+  }, function(items) {
 
-    try {
-      // Try and use browser's smooth scrolling if available
-      window.scroll({
-        top: scrollPosition,
-        behavior: "smooth"
-      });
-    } catch (e) {
-      // Fallback
-      smoothScroll(scrollPosition);
+    // If top of comment is out of viewport, scroll to it
+    if (rect.top < 0) {
+      const padding = 10;
+      let scrollPosition = (window.scrollY + rect.top) - padding;
+
+      if (items.collapseAnimation) {
+
+        try {
+          // Try and use browser's smooth scrolling if available
+          window.scroll({
+            top: scrollPosition,
+            behavior: "smooth"
+          });
+        } catch (e) {
+          // Fallback
+          smoothScroll(scrollPosition);
+        }
+
+      } else {
+
+        window.scrollTo(null, scrollPosition);
+
+      }
+
     }
-  }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  chrome.storage.sync.get({
+    style: 'minimalist'
+  }, function(items) {
+
+    if (items.style === 'minimalist') {
+
+      document.body.classList.add('rcc-minimalist');
+
+    } else {
+
+      document.body.classList.add('rcc-retro');
+      injectRetroTheme();
+
+    }
+
+  });
+
 });
